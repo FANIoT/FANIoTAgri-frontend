7d7edd3a13ae5e1c799b2ed9ab13123f2fe35b82
Parham Alvani <parham.alvani@gmail.com>
2018-12-29 21:05:24 +0330

    Registers navigation component and bootstrap angular module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { SensorsComponent } from './sensors/sensors.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
+    NavComponent,
     SensorsComponent
   ],
   imports: [
     BrowserModule,
-    LeafletModule.forRoot()
+    LeafletModule.forRoot(),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
