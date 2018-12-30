41fa39c77fada4c954ed74c1a5f60f9bc0c7f0b0
Parham Alvani <parham.alvani@gmail.com>
2018-12-30 09:33:42 +0330

    Registers calender component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,34 @@ import { NgModule } from '@angular/core';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { SensorsComponent } from './sensors/sensors.component';
 import { NavComponent } from './nav/nav.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
     NavComponent,
-    SensorsComponent
+    SensorsComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
     LeafletModule.forRoot(),
     NgbModule,
-    ScrollToModule.forRoot()
+    ScrollToModule.forRoot(),
+    BrowserAnimationsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
