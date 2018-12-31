0ee94809d6dd70dfa48a899c9e64155860335715
Parham Alvani <parham.alvani@gmail.com>
2018-12-31 21:04:13 +0330

    Moves modules into main modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CalendarModule, DateAdapter } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-import { MatTabsModule } from '@angular/material/tabs';
+import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { MapComponent } from './map/map.component';
-import { SensorsComponent } from './sensors/sensors.component';
-import { NavComponent } from './nav/nav.component';
-import { CalendarComponent } from './calendar/calendar.component';
-import { QuoteComponent } from './quote/quote.component';
-import { FooterComponent } from './footer/footer.component';
-import { MapRsComponent } from './map-rs/map-rs.component';
+import { LoginComponent } from './login/login.component';
+
+import { MainModule } from './main/main.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent,
-    NavComponent,
-    SensorsComponent,
-    CalendarComponent,
-    QuoteComponent,
-    FooterComponent,
-    MapRsComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    LeafletModule.forRoot(),
-    NgbModule,
-    ScrollToModule.forRoot(),
     BrowserAnimationsModule,
-    MatTabsModule,
-    CalendarModule.forRoot({
-      provide: DateAdapter,
-      useFactory: adapterFactory
-    })
+    FormsModule,
+    AppRoutingModule,
+    MainModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
