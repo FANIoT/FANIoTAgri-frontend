0d0d62b041a2151daabacc1cabfb452b3f8efd3e
Parham Alvani <parham.alvani@gmail.com>
2018-12-31 21:03:27 +0330

    Adds routes.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
