7a79ac7ffa26a9b33230fc6e69d5165e6f60dac7
Parham Alvani <parham.alvani@gmail.com>
2019-01-05 20:52:26 +0330

    Adds route for sirjan.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MainComponent } from './main/main.component';
+import { MainComponent as SirjanComponent } from './sirjan/main.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'main', component: MainComponent },
+  { path: 'sirjan', component: SirjanComponent },
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
